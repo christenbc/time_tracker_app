f7abc90ae3285a0d51fb21c15d467b35d540f0e0
chbloca <chris10bdc@gmail.com>
2021-03-27 19:43:40 +0100

    Extract container (blank page) to a sign_in_page

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget { // alt+Ent to show dependencies to red variables
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
