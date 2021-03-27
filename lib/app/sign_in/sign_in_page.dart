e2aee19b4bf66390106983f3ac87aa9dfd1ee5f4
chbloca <chris10bdc@gmail.com>
2021-03-27 21:38:13 +0100

    Substitute the other two boxes for a button with text

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -29,20 +29,10 @@ class SignInPage extends StatelessWidget {
             ),
           ),
           SizedBox(height: 8.0),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100.0,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100.0,
-            ),
-          ),
-          SizedBox(height: 8.0),
+          RaisedButton(
+            child: Text('Sign in with Google'),
+            onPressed: () {},
+          )
         ],
       ),
     );
