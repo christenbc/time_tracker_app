806d154a20b03aacc0bef67165a24466703e590a
chbloca <chris10bdc@gmail.com>
2021-03-27 20:29:16 +0100

    Extract body content into a method

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -9,31 +9,35 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100.0,
-              ),
+      body: buildContent(),
+    );
+  }
+
+  Container buildContent() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100.0,
             ),
-            Container(
-              color: Colors.red,
-              child: SizedBox(
-                height: 100.0,
-              ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
             ),
-            Container(
-              color: Colors.purple,
-              child: SizedBox(
-                height: 100.0,
-              ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
