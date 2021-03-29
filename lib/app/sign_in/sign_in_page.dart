048dd2324a46b1d854d4c15d8fa202ac29936809
chbloca <chris10bdc@gmail.com>
2021-03-29 19:33:34 +0200

    wrap rightmost image into Opacity widget and set it to transparent

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -35,6 +35,10 @@ class SignInPage extends StatelessWidget {
               children: [
                 Image.asset('images/google-logo.png'),
                 Text('Sign in with Google'),
+                Opacity(
+                  opacity: 0.0,
+                  child: Image.asset('images/google-logo.png'),
+                ),
               ],
             ),
             color: Colors.white,
