d7ade106a25493754daf4fb8d4017446d119f80e
chbloca <chris10bdc@gmail.com>
2021-04-04 13:42:17 +0200

    copy and paste _signInAnonymously method

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  Future<void> _signInAnonymously() async {
+    try {
+      // good practice to asynchronous apis
+      final userCredentials = await FirebaseAuth.instance.signInAnonymously(); // since it returns a Future (see doc), add await and async. it is good practice to add Future <void>
+      onSignIn(userCredentials.user);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
