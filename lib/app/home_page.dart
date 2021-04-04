4940d72e01389221797b63890970f5ee3f308dce
chbloca <chris10bdc@gmail.com>
2021-04-04 13:51:45 +0200

    implement callback of the sign out method

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -2,11 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key key, @required this.onSignOut}) : super(key: key);
+  final VoidCallback onSignOut;
 
   Future<void> _signOut() async {
     try {
       await FirebaseAuth.instance.signOut(); // since it returns a Future (see doc), add await and async. it is good practice to add Future <void>
-
+      onSignOut();
     } catch (e) {
       print(e.toString());
     }
