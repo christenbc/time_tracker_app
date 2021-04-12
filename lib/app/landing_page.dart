a1d06420ca95b8d9aed433ddecbce02c4adf0361
chbloca <chris10bdc@gmail.com>
2021-04-12 12:39:53 +0200

    with StreamBuilder, there is code which is not needed anymore. besides, now it is possible to turn the stateful widget into a stateless widget, so additional unused code is deleted

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -5,54 +5,32 @@ import 'package:time_tracker/app/home_page.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker/services/auth.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   const LandingPage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
 
   @override
-  _LandingPageState createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-
-  User _user;
-
-  @override
-  void initState() {
-    super.initState();
-    _updateUser(widget.auth.currentUser); // add widget. so that auth variable is accessible
-  }
-
-  void _updateUser(User user){
-    setState(() {
-      _user = user;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: widget.auth.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active){
-            final user = snapshot.data;
-            if (user == null){
-              return SignInPage(
-                auth: widget.auth,
-                onSignIn: (user) =>_updateUser(user), // the same as onSignIn: _updateUser
-              );
-            }
-            return HomePage(
-              auth: widget.auth,
-              onSignOut: () => _updateUser(null),
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          if (user == null) {
+            return SignInPage(
+              auth: auth,
             );
           }
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
+          return HomePage(
+            auth: auth,
           );
         }
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
     );
   }
 }
