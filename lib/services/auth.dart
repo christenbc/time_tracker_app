fdfc1a5d8fe03bf1fe465421b8ad474ea1081a48
chbloca <chris10bdc@gmail.com>
2021-04-12 10:28:14 +0200

    add overrides to every correlated method as a good practice

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -11,13 +11,16 @@ class Auth implements AuthBase{
 
   final _firebaseAuth = FirebaseAuth.instance;
 
+  @override
   User get currentUser => FirebaseAuth.instance.currentUser;
 
+  @override
   Future<User> signInAnonymously() async {
      final userCredential = await _firebaseAuth.signInAnonymously();
      return userCredential.user;
   }
 
+  @override
   Future<void> signOut() async{
     await _firebaseAuth.signOut();
   }
