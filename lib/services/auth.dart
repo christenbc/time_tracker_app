5f233890a650d75bdcfefda3745801cbbe9f602e
chbloca <chris10bdc@gmail.com>
2021-04-12 10:24:56 +0200

    create an abstract class to make a public interface that lists all the methods

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,11 @@
-
-
 import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthBase {
+
+  User get currentUser;
+  Future<User> signInAnonymously();
+  Future<void> signOut();
+}
 
 class Auth{
 
