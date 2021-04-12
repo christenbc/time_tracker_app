914940e0e462862465c80d37459569b74228290f
chbloca <chris10bdc@gmail.com>
2021-04-12 13:29:54 +0200

    implement signInWithGoogle

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User get currentUser;
   Stream<User> authStateChanges();
   Future<User> signInAnonymously();
+  Future<User> signInWithGoogle();
   Future<void> signOut();
 }
 
-class Auth implements AuthBase{
-
+class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
 
   @override
@@ -19,13 +21,41 @@ class Auth implements AuthBase{
 
   @override
   Future<User> signInAnonymously() async {
-     final userCredential = await _firebaseAuth.signInAnonymously();
-     return userCredential.user;
+    final userCredential = await _firebaseAuth.signInAnonymously();
+    return userCredential.user;
   }
 
   @override
-  Future<void> signOut() async{
-    await _firebaseAuth.signOut();
+  Future<User> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.credential(
+            idToken: googleAuth.idToken,
+            accessToken: googleAuth.accessToken,
+          ),
+        );
+        return userCredential.user;
+      }
+      else {
+        throw FirebaseAuthException(
+          code: 'ERROR_MISSING_GOOGLE_ID_TOKEN',
+          message: 'Missing Google ID Token',
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
   }
 
+  @override
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
 }
