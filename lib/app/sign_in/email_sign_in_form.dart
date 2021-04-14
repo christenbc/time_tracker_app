2fc5cbed537bdee34d0e81d2d3209656b93a5390
chbloca <chris10bdc@gmail.com>
2021-04-14 17:18:25 +0200

    add Column implementation

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  List<Widget> _buildChildren(){
+    return [];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [],
+    );
   }
 }
