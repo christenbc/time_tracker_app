6edf7b66f0a9c518f0e5d694bd7eba4f71fb27f9
chbloca <chris10bdc@gmail.com>
2021-04-14 17:20:58 +0200

    add input email

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 
 class EmailSignInForm extends StatelessWidget {
   List<Widget> _buildChildren(){
-    return [];
+    return [
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'text@test.com',
+        ),
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      children: _buildChildren(),
     );
   }
 }
