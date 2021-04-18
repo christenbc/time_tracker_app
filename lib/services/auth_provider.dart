0f27c1f5c3cf59387745708aafe08476902196a2
chbloca <chris10bdc@gmail.com>
2021-04-18 14:14:27 +0200

    create auth provider class and insert one missing override

---
+++ lib/services/auth_provider.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class AuthProvider extends InheritedWidget{ // insert 1 missing override
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    throw UnimplementedError();
+  }
+
+}
