9901e817c7439562c9e0c8e2e0efe0d45393066a
chbloca <chris10bdc@gmail.com>
2021-04-18 16:38:12 +0200

    create more human readable dialog exceptions

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker/app/sign_in/validators.dart';
@@ -56,11 +57,11 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
         await auth.createUserWithEmailAndPassword(_email, _password);
       }
       Navigator.of(context).pop();
-    } catch (e) {
+    } on FirebaseAuthException catch (e) {
       showAlertDialog(
           context,
           title: 'Sign in failed',
-          content: e.toString(),
+          content: e.message,
           defaultActionText: 'OK');
     } finally {
       setState(() {
