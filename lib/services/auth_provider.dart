eb732546679aa24339b4ca8179eed7a0d46d5bc4
chbloca <chris10bdc@gmail.com>
2021-04-18 14:22:54 +0200

    finish AuthProvider implementation

--- lib/services/auth_provider.dart
+++ lib/services/auth_provider.dart
@@ -1,12 +1,19 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:time_tracker/services/auth.dart';
 
 class AuthProvider extends InheritedWidget{ // insert 1 missing override
-  @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    // TODO: implement updateShouldNotify
-    throw UnimplementedError();
-  }
+  AuthProvider({@required this.auth, @required this.child});
+  final AuthBase auth;
+  final Widget child;
 
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => false;
+
+  // final auth = AuthProvider.of(context;
+  static AuthBase of(BuildContext context) {
+    AuthProvider provider = context.dependOnInheritedWidgetOfExactType<AuthProvider>();
+    return provider.auth;
+  }
 }
