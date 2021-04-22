428d18d338420ba107f81d46d95613d5c6922bc8
chbloca <chris10bdc@gmail.com>
2021-04-22 11:09:01 +0200

    creating a model class for the EmailSignInForm

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:time_tracker/app/sign_in/email_sign_in_form.dart';
+import 'package:time_tracker/app/sign_in/email_sign_in_form_stateful.dart';
 
 class EmailSignInPage extends StatelessWidget {
   @override
@@ -14,7 +14,7 @@ class EmailSignInPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Card(
-            child: EmailSignInForm(),
+            child: EmailSignInFormStateful(),
           ),
         ),
       ),
