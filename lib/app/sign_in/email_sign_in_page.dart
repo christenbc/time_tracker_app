7753e63f6194a1f4a9f0e3a5bd98e654550f607e
chbloca <chris10bdc@gmail.com>
2021-04-22 12:27:55 +0200

    setting up the EmailSignInFormBlocBased with Provider

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/email_sign_in_form_bloc_based.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_form_stateful.dart';
 
 class EmailSignInPage extends StatelessWidget {
@@ -14,7 +15,7 @@ class EmailSignInPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Card(
-            child: EmailSignInFormStateful(),
+            child: EmailSignInFormBlocBased.create(context),
           ),
         ),
       ),
