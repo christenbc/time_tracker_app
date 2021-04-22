c060277ab944a832ffc4162df07327a39384078b
chbloca <chris10bdc@gmail.com>
2021-04-22 12:00:35 +0200

    updating the model

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -2,10 +2,32 @@ import 'dart:async';
 import 'package:time_tracker/app/sign_in/email_sign_in_model.dart';
 
 class EmailSignInBloc {
-  final StreamController<EmailSignInModel> _modelController = StreamController<EmailSignInModel>();
+  final StreamController<EmailSignInModel> _modelController =
+      StreamController<EmailSignInModel>();
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
+  EmailSignInModel _model = EmailSignInModel();
 
-  void dispose(){
+  void dispose() {
     _modelController.close();
+  }
+
+  void updateWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    // update model
+    _model = _model.copyWith(
+      email: email,
+      password: password,
+      formType: formType,
+      isLoading: isLoading,
+      submitted: submitted,
+    );
+
+    // add updated model to _modelController
+    _modelController.add(_model);
   }
 }
--- lib/app/sign_in/email_sign_in_model.dart
+++ lib/app/sign_in/email_sign_in_model.dart
@@ -9,9 +9,28 @@ class EmailSignInModel {
     this.submitted = false,
   });
 
+  // should be immutable
   final String email;
   final String password;
   final EmailSignInFormType formType;
   final bool isLoading;
   final bool submitted;
+
+  EmailSignInModel copyWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    return EmailSignInModel(
+      email: email ?? this.email,
+      // conditional method which returns the value of the left if it's not null,
+      // otherwise it pass the right side
+      password: password ?? this.password,
+      formType: formType ?? this.formType,
+      isLoading: isLoading ?? this.isLoading,
+      submitted: submitted ?? this.submitted,
+    );
+  }
 }
