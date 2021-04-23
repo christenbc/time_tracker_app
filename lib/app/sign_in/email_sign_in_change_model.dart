44cddd17d42708b655af47878f0c466354cf7acc
chbloca <chris10bdc@gmail.com>
2021-04-23 11:24:09 +0200

    completing the EmailSignInChangeModel class

--- lib/app/sign_in/email_sign_in_change_model.dart
+++ lib/app/sign_in/email_sign_in_change_model.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/foundation.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_model.dart';
 import 'package:time_tracker/app/sign_in/validators.dart';
+import 'package:time_tracker/services/auth.dart';
 
 class EmailSignInChangeModel with EmailAndPasswordValidators, ChangeNotifier {
   EmailSignInChangeModel({
+    @required this.auth,
     this.email = '',
     this.password = '',
     this.formType = EmailSignInFormType.signIn,
     this.isLoading = false,
     this.submitted = false,
   });
-
+  final AuthBase auth;
   // should be mutable
   String email;
   String password;
   EmailSignInFormType formType;
   bool isLoading;
   bool submitted;
+
+  Future<void> submit() async {
+    updateWith(submitted: true, isLoading: true);
+    try {
+      // await Future.delayed(Duration(seconds: 3));
+      if (formType == EmailSignInFormType.signIn) {
+        await auth.signInWithEmailAndPassword(email, password);
+      } else {
+        await auth.createUserWithEmailAndPassword(email, password);
+      }
+    } catch (e) {
+      updateWith(isLoading: false);
+      rethrow;
+    }
+  }
 
   String get primaryButtonText {
     return formType == EmailSignInFormType.signIn
@@ -47,6 +64,21 @@ class EmailSignInChangeModel with EmailAndPasswordValidators, ChangeNotifier {
         submitted && !emailValidator.isValid(email);
     return showErrorText ? invalidEmailErrorText : null;
   }
+
+  void toggleFormType() {
+    final formType = this.formType == EmailSignInFormType.signIn
+        ? EmailSignInFormType.register
+        : EmailSignInFormType.signIn;
+    updateWith(
+      email: '',
+      password: '',
+      isLoading: false,
+      submitted: false,
+      formType: formType,
+    );
+  }
+  void updateEmail(String email) => updateWith(email: email);
+  void updatePassword(String password) => updateWith(password: password);
 
   void updateWith({
     String email,
