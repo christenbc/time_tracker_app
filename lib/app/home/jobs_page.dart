47d4d0a6d95fc3e2c96fb938682fcb5ce9597f5f
chbloca <chris10bdc@gmail.com>
2021-04-23 20:28:10 +0200

    renaming the HomePage to JobsPage

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:time_tracker/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker/services/auth.dart';
 
-class HomePage extends StatelessWidget {
+class JobsPage extends StatelessWidget {
   Future<void> _signOut(BuildContext context) async {
     try {
       final auth = Provider.of<AuthBase>(context, listen: false);
@@ -31,7 +31,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Jobs'),
+        centerTitle: true,
         actions: [
           FlatButton(
             child: Text(
--- lib/common_widgets/show_exception_alert_dialog.dart
+++ lib/common_widgets/show_exception_alert_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:time_tracker/common_widgets/show_alert_dialog.dart';
 
 Future<void> showExceptionAlertDialog(
