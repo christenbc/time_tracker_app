4ed3d7bfab3e5211bc38d07a4b124bab3a795fcc
chbloca <chris10bdc@gmail.com>
2021-04-23 20:35:41 +0200

    adding the database class

---
+++ lib/services/database.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+abstract class Database {
+
+}
+
+class FirestoreDatabase implements Database {
+  FirestoreDatabase({@required this.uid}) : assert(uid != null);
+  final String uid;
+}
