4ef3f5042ffb7505914d693b3f8fb82f830663f1
chbloca <chris10bdc@gmail.com>
2021-04-27 11:21:01 +0200

    deleting jobs from firestore

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,6 +5,7 @@ import 'package:time_tracker/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> setJob(Job job);
+  Future<void> deleteJob(Job job);
   Stream<List<Job>> jobsStream();
 }
 
@@ -19,11 +20,18 @@ class FirestoreDatabase implements Database {
   // final _service = FirestoreService();
   final _service = FirestoreService.instance;
 
+  @override
   Future<void> setJob(Job job) => _service.setData(
     path: APIPath.job(uid, job.id),
     data: job.toMap(),
   );
 
+  @override
+  Future<void> deleteJob(Job job) async => _service.deleteData(
+      path: APIPath.job(uid, job.id)
+  );
+
+  @override
   Stream<List<Job>> jobsStream() => _service.collectionStream(
       path: APIPath.jobs(uid),
       builder: (data, documentId) => Job.fromMap(data, documentId),
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -6,21 +6,30 @@ class FirestoreService {
   FirestoreService._();
   static final instance = FirestoreService._();
 
-  Future<void> setData({String path, Map<String, dynamic> data}) async {
+  Future<void> setData({
+    @required String path,
+    @required Map<String, dynamic> data,
+  }) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('$path: $data');
     await reference.set(data);
   }
 
-  Stream<List<T>> collectionStream<T>({
-    @required String path,
-    @required T Function(Map<String, dynamic> data, String id) builder
-  }) {
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs.map(
-          (snapshot) => builder(snapshot.data(), snapshot.id),
-    ).toList());
+  Future<void> deleteData({@required String path}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('delete : $path');
+    await reference.delete();
   }
 
+  Stream<List<T>> collectionStream<T>(
+      {@required String path,
+      @required T Function(Map<String, dynamic> data, String id) builder}) {
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => snapshot.docs
+        .map(
+          (snapshot) => builder(snapshot.data(), snapshot.id),
+        )
+        .toList());
+  }
 }
