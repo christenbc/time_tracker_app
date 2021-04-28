f987e9f70eebb4e3d3cce5b97cf816af5ca863f3
chbloca <chris10bdc@gmail.com>
2021-04-28 18:07:44 +0200

    replacing the floating action buttons

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -42,6 +42,13 @@ class JobsPage extends StatelessWidget {
         title: Text('Jobs'),
         centerTitle: true,
         actions: [
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.white),
+            onPressed: () => EditJobPage.show(
+              context,
+              database: Provider.of<Database>(context, listen: false),
+            ),
+          ),
           FlatButton(
             child: Text(
               'Logout',
@@ -52,13 +59,6 @@ class JobsPage extends StatelessWidget {
         ],
       ),
       body: _buildContents(context),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => EditJobPage.show(
-          context,
-          database: Provider.of<Database>(context, listen: false),
-        ),
-      ),
     );
   }
 
