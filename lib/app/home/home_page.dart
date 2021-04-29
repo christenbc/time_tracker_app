b815936c99ee68d71baede9c685bd28466b07d37
chbloca <chris10bdc@gmail.com>
2021-04-29 10:40:57 +0200

    adding the source code for the new entries page

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/home/account/account_page.dart';
 import 'package:time_tracker/app/home/cupertino_home_scaffold.dart';
+import 'package:time_tracker/app/home/entries/entries_page.dart';
 import 'package:time_tracker/app/home/jobs/jobs_page.dart';
 import 'package:time_tracker/app/home/tab_item.dart';
 
@@ -25,7 +26,7 @@ class _HomePageState extends State<HomePage> {
   Map<TabItem, WidgetBuilder> get widgetBuilders {
     return {
       TabItem.jobs: (_) => JobsPage(),
-      TabItem.entries: (_) => Container(),
+      TabItem.entries: (context) => EntriesPage.create(context),
       TabItem.account: (_) => AccountPage(),
     };
   }
