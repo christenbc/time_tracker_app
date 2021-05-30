b903d5930db609d274064e0c563c581e203a5b87
chbloca <chris10bdc@gmail.com>
2021-05-30 13:21:40 +0200

    initialized test CustomRaisedButton widget

--- test/custom_raised_button_test.dart
+++ test/custom_raised_button_test.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker/common_widgets/custom_raised_button.dart';
 
 void main() {
-  testWidgets('', (WidgetTester tester) async {});
+  testWidgets('', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: CustomRaisedButton()));
+  });
 }
