0e5a8ac55847e7688ccc1026eb7ad5f015966b38
chbloca <chris10bdc@gmail.com>
2021-05-30 14:20:38 +0200

    verifiying mock methods

--- test/email_sign_in_form_stateful_test.dart
+++ test/email_sign_in_form_stateful_test.dart
@@ -5,6 +5,8 @@ import 'package:provider/provider.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_form_stateful.dart';
 import 'package:time_tracker/services/auth.dart';
 
+// GIVEN user is on the email sign in page
+
 class MockAuth extends Mock implements AuthBase {}
 
 void main() {
@@ -26,4 +28,17 @@ void main() {
       ),
     );
   }
+
+  testWidgets(
+      'WHEN user doesn\'t enter the email and password '
+      'AND user taps on the sign-in button '
+      'THEN signInWithEmailAndPassword is not called ',
+      (WidgetTester tester) async {
+    await pumpEmailSignInForm(tester);
+
+    final signInButton = find.text('Sign in');
+    await tester.tap(signInButton);
+
+    verifyNever(mockAuth.signInWithEmailAndPassword(any, any));
+  });
 }
