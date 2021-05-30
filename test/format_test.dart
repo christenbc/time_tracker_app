3473fa0bf941601aa063d7efb3fc648c5cd49bc7
chbloca <chris10bdc@gmail.com>
2021-05-30 12:00:07 +0200

    completing the formatting tests

--- test/format_test.dart
+++ test/format_test.dart
@@ -30,5 +30,52 @@ void main() {
         '12 Aug 2019',
       );
     });
+    test('2019-08-16', () {
+      expect(
+        Format.date(DateTime(2019, 8, 16)),
+        '16 Aug 2019',
+      );
+    });
+  });
+
+  group('dayOfWeek - GB locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_GB';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('Monday', () {
+      expect(
+        Format.dayOfWeek(DateTime(2019, 8, 12)),
+        'Mon',
+      );
+    });
+  });
+
+  group('dayOfWeek - IT locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'it_IT';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('Lunedi', () {
+      expect(
+        Format.dayOfWeek(DateTime(2019, 8, 12)),
+        'lun',
+      );
+    });
+  });
+
+  group('currency - US locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_US';
+    });
+    test('positive', () {
+      expect(Format.currency(10), '\$10');
+    });
+    test('zero', () {
+      expect(Format.currency(0), '');
+    });
+    test('negative', () {
+      expect(Format.currency(-5), '-\$5');
+    });
   });
 }
